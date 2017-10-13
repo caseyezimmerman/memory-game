61b25e8d59739b6dd4df31de0dcf824fb96b937a
Casey Zimmerman <caseyezimmerman@gmail.com>
2017-10-13 15:08:38 -0400

    second

--- memoryscript.js
+++ memoryscript.js
@@ -2,29 +2,58 @@ $(document).ready(function(){
 console.log("test")
 
 	const cards = [
-		'<img src="images/monsters-01.png">',
-		'<img src="images/monsters-02.png">',
-		'<img src="images/monsters-03.png">',
-		'<img src="images/monsters-04.png">',
-		'<img src="images/monsters-05.png">',
-		'<img src="images/monsters-06.png">',
-		'<img src="images/monsters-07.png">',
-		'<img src="images/monsters-08.png">',
-		'<img src="images/monsters-09.png">',
+		'<img src="newimages/falcons.png">',
+		'<img src="newimages/football.png">',
+		'<img src="newimages/mattryan.png">',
+		'<img src="newimages/juliojones.png">',
+		'<img src="newimages/sanu.png">',
+		'<img src="newimages/helmet.png">',
+		'<img src="newimages/statue.png">',
+		'<img src="newimages/braves.png">',
+		'<img src="newimages/atlunited.png">',
 		// '<img src="images/monsters-10.png">',
-		'<img src="images/monsters-11.png">',
+		'<img src="newimages/almiron.png">',
 		// '<img src="images/monsters-12.png">',		
-		'<img src="images/monsters-13.png">',		
-		'<img src="images/monsters-14.png">',		
-		'<img src="images/monsters-15.png">',		
-		'<img src="images/monsters-16.png">',				
+		'<img src="newimages/asad.png">',		
+		'<img src="newimages/guzan.jpeg">',		
+		'<img src="newimages/soccerball.png">',		
+		'<img src="newimages/hawks.png">',				
 	];
-
+	
+	var freshCards = cards.slice()
 
 	var gridSize = 4;
 	var cardsToUse = [];
 
+
+
+
+
+	//MAKING A COUNT UP TIMER TO TIME THE USER ONN HOW LONG IT TAKES THEM TO FINISH
+	//STARTS ONCE THEY CHOOSE THEIR DIFFICULTY
+	var test;
+	$("button").click(function(){
+		var totalSeconds = 0;
+
+		test = setInterval(function(){
+			++totalSeconds;
+			var seconds = totalSeconds
+			$("#timer").html(seconds)
+		}, 1000);
+
+	});
+	//STOPS ONCE THEY CLICK ON STOP TIMER
+	$("#stop-timer").click(function(event){
+		clearInterval(test);
+		console.log("click")
+	});
+
+	
+
+	
+	//TELLING THE DOM WHAT TO DO WHEN EACH BUTTON IS CLICKED
 	$('button').click(function(){
+		console.log(freshCards)
 		// console.log("user clicked button")
 		var userAnswer = $(this).html();
 		// console.log(userAnswer)
@@ -36,12 +65,40 @@ console.log("test")
 			gridSize = 28
 		}
 
-		// for(let i = 0; i < gridSize; i++){
+
+		//PUTTING 2 RANDOM CARD INTO THE CARDS TO USE ARRAY
+		for(let i = 0; i < gridSize/2; i++){
+			var rand = cards[Math.floor(Math.random()*cards.length)];
+			cardsToUse.push(rand, rand)
+			// console.log(cardsToUse)
+		}
+		//SHUFFLING THE CARDS WE JUST TOLD IT TO PUT IN CARDS TO USE ARRAY
+		cardsToUse = shuffleCards(cardsToUse);
+
+
+		//THE SHUFFLE CARD FUNCTION
+		function shuffleCards(cardsToShuffle){
+			for(let i = 0; i < 5000; i++){
+				rand1 = Math.floor(Math.random()*cardsToShuffle.length);
+				rand2 = Math.floor(Math.random()*cardsToShuffle.length);
+				var temp = cardsToShuffle[rand1];
+				cardsToShuffle[rand1] = cardsToShuffle[rand2];
+				cardsToShuffle[rand2] = temp;
+
+			}
+			return cardsToShuffle
+		}
+
+		
+
 
 		// }
-		cards.map(function(card){
-			cardsToUse.push(card,card) //the SAME card gets pushed onto the array twice, thats how we get two of each image on the 
-		})
+		// cards.map(function(card){
+		// 	cardsToUse.push(card, card) //the SAME card gets pushed onto the array twice, thats how we get two of each image on the 
+		// })
+
+
+
 
 		var memoryHTML = "";
 		// ---adds the memoryHTML as many times as grid size
@@ -80,9 +137,11 @@ console.log("test")
 					})
 					// cardsUp.addClass('matched'); can use this unstead of function bc we assigned cardsUp to as array of elemnts with the class flip
 				}else{
+					cardsUp.removeClass("flip");
+					cardsUp.addClass('temp-flip')
 					// these are not a match bc the html is different
 					setTimeout(function(){ //waits 2 seconds until it removes the class flip of the card
-						cardsUp.removeClass('flip');
+						cardsUp.removeClass('temp-flip');
 					},1000)
 				}
 			}else{
